8e7ee72137ed81d051e2a3b13e7902e3ec497b69
nina1666 <105611148+nina1666@users.noreply.github.com>
2023-02-10 10:59:23 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,7 @@
-class holaMundo
+class HolaMundo
 {
-    // Tu programa comienza con una llamada a main().
-    // Imprime "Hola Mundo" a la ventana de la terminal.
     public static void main(String args[])
     {
-          
         System.out.println("Hola Mundo");
     }
 }
